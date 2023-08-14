cd5ea467b294cc3b36f36a977bb2d973153c71ec
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-08-14 14:03:01 +0100

    Deleting workouts

--- src/components/WorkoutDetails.jsx
+++ src/components/WorkoutDetails.jsx
@@ -1,5 +1,17 @@
+import axios from "axios"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+
 /* eslint-disable react/prop-types */
 const WorkoutDetails = ({ workout }) => {
+	const { dispatch } = useWorkoutsContext()
+	const handleClick = async () => {
+		const response = await axios.delete(`http://localhost:4000/api/workouts/${workout._id}`)
+		const data = response.data
+		if (response.statusText == "OK") {
+			dispatch({ type: "DELETE_WORKOUT", payload: data.workout })
+			console.log(response)
+		}
+	}
 	return (
 		<>
 			<div className="workout-details">
@@ -12,6 +24,7 @@ const WorkoutDetails = ({ workout }) => {
 					<strong>Reps:</strong> {workout.reps}
 				</p>
 				<p>{workout.createdAt}</p>
+				<span onClick={handleClick}>Delete</span>
 			</div>
 		</>
 	)
